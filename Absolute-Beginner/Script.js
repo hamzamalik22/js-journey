28f61dc31bdbb65ecc8bb63f05a68870ae8f7e64
hamzamalik22 <hamzamalikaj22@gmail.com>
2024-04-15 18:02:17 +0500

    var & consts

---
+++ Absolute-Beginner/Script.js
@@ -0,0 +1,35 @@
+//  ---------------------- Browser Builtin -----------------------
+// // console, alert, prompt
+
+// // Console (.log, .warn, .error)
+
+// console.log("Hamza Malik") // Used to print values in console of browser
+// console.warn("Hamza Malik") // Used to print values in console of browser as a warning!
+// console.error("Hamza Malik") // Used to print values in console of browser as an error🚫
+
+// // Alert
+// alert("Don't move!") // Give a pop up warning on browser when it is refreshed
+
+// // Prompt
+// prompt("What is Your name?") // Used to get user input 
+
+
+//  ---------------------- Variable & Constants -----------------------
+
+
+var age = 20;
+
+var myName = 'Hamza';
+
+const id = 10101;
+
+console.log(myName);  // Hamza
+console.log(age);    // 20
+console.log(id);    // 10101
+
+age = 21; 
+id = 10102; 
+
+console.log(age) // 21 
+console.log(id) // will throw error as constants are immutable (Can't be changed)
+                // Uncaught TypeError: Assignment to constant variable.
