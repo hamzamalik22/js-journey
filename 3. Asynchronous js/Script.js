0e338304e11e686317e05b72705b31ec87090617
hamzamalik22 <hamzamalikaj22@gmail.com>
2024-04-19 16:06:55 +0500

    Fetch api & Axios

--- 3. Asynchronous js/Script.js
+++ 3. Asynchronous js/Script.js
@@ -1,27 +1,38 @@
 //  ---------------------- 1. SetTimeout -----------------------
 
-setTimeout(function(){
-    console.log('Hamza') // this will print Hamza after 2 seconds - es5
-},2000)
+// setTimeout(function(){
+//     console.log('Hamza') // this will print Hamza after 2 seconds - es5
+// },2000)
 
-setTimeout(()=>{
-    console.log('Hamza') // this will print Hamza after 2 seconds - es6
-},2000)
-
-
-//  ---------------------- 1. SetInterval -----------------------
-
-setInterval(()=>{
-    console.log('Hamza Malik') // this will continuously print Hamza after 2 seconds (loop) 
-},2000)
-
-let count = 0;
-let stop = setInterval(()=>{
-    count++;
-    console.log(count) // this will continuously print count after 2 seconds (loop)
-    if (count === 6){
-        clearInterval(stop); // this will stop interval's loop when condition verified..
-    }
-},2000)
+// setTimeout(()=>{
+//     console.log('Hamza') // this will print Hamza after 2 seconds - es6
+// },2000)
 
 
+//  ---------------------- 2. SetInterval -----------------------
+
+// setInterval(()=>{
+//     console.log('Hamza Malik') // this will continuously print Hamza after 2 seconds (loop) 
+// },2000)
+
+// let count = 0;
+// let stop = setInterval(()=>{
+//     count++;
+//     console.log(count) // this will continuously print count after 2 seconds (loop)
+//     if (count === 6){
+//         clearInterval(stop); // this will stop interval's loop when condition verified..
+//     }
+// },2000)
+
+
+//  ---------------------- 3. Fetch api -----------------------
+
+fetch(`https://randomuser.me/api/`)
+.then(raw => raw.json())
+.then(response => console.log(response.results[0].name.first,response.results[0].name.last ))
+
+
+//  ---------------------- 4. Axios -----------------------
+
+axios.get(`https://randomuser.me/api/`)
+.then(response => console.log(response.data.results[0].name.first,response.data.results[0].name.last))
