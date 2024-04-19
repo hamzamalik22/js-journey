614976eeb82de80f9d0163fea738f8535ab2b35b
hamzamalik22 <hamzamalikaj22@gmail.com>
2024-04-19 23:46:22 +0500

    Selectors in Dom

---
+++ 4. Dom/Script.js
@@ -0,0 +1,9 @@
+//     Document selectors
+
+let a =  document.getElementById('#head1')
+
+let b = document.getElementsByClassName('.head2')
+
+let c = document.getElementsByTagName('h3')
+
+let d = document.querySelector('h4');
