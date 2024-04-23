0f60a69b16c659f4f4d7b3dee9e563f6e4a5f12c
hamzamalik22 <hamzamalikaj22@gmail.com>
2024-04-23 20:11:21 +0500

    Local Storage in js

--- 6. Developer Problems/Script.js
+++ 6. Developer Problems/Script.js
@@ -10,27 +10,56 @@
 
 // 2. Handling Asynchronous call
 
-let btn = document.querySelector('button');
+// let btn = document.querySelector('button');
 
-function getUser(){
-    fetch(`https://randomuser.me/api/`)
-    .then((raw) => raw.json())
-    .then((result) => {
-        const { name, email, gender, picture } = result.results[0];
-        document.querySelector(
-            "#parent"
-        ).innerHTML += `<div class="card w-60 p-4 rounded-xl bg-zinc-800">
-    <div class="w-20 h-20 bg-zinc-500 rounded-2xl mb-3 overflow-hidden">
-        <img src="${picture.large}" alt="">
-    </div>
-    <h3 class="font-semibold text-2xl">${name.first}</h3>
-    <h5 class="text-sm font-semibold opacity-60">${email}</h5>
-    <h3 class="text-sm opacity-40">${gender}</h3>
-    <h6 class="mt-5 text-xs font-semibold opacity-80">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime sunt dolor enim recusandae?</h6>
-</div>`;
-});
+// function getUser(){
+//     fetch(`https://randomuser.me/api/`)
+//     .then((raw) => raw.json())
+//     .then((result) => {
+//         const { name, email, gender, picture } = result.results[0];
+//         document.querySelector(
+//             "#parent"
+//         ).innerHTML += `<div class="card w-60 p-4 rounded-xl bg-zinc-800">
+//     <div class="w-20 h-20 bg-zinc-500 rounded-2xl mb-3 overflow-hidden">
+//         <img src="${picture.large}" alt="">
+//     </div>
+//     <h3 class="font-semibold text-2xl">${name.first}</h3>
+//     <h5 class="text-sm font-semibold opacity-60">${email}</h5>
+//     <h3 class="text-sm opacity-40">${gender}</h3>
+//     <h6 class="mt-5 text-xs font-semibold opacity-80">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime sunt dolor enim recusandae?</h6>
+// </div>`;
+// });
+// }
+
+// btn.addEventListener('click',()=>{
+//     getUser();
+// })
+
+
+// 3. Local Storage
+
+let block = document.querySelector('#block');
+let unblock = document.querySelector('#unblock');
+// let status = document.querySelector('#status');
+
+block.addEventListener('click',()=>{
+    localStorage.setItem('block',true);
+    showStatus();
+})
+
+unblock.addEventListener('click',()=>{
+    localStorage.setItem('block',false);
+    showStatus();
+})
+
+function showStatus(){
+    if (localStorage.getItem('block') === 'false' ){
+        document.querySelector('#status span').textContent = 'Not Blocked';
+    }
+    else{
+        document.querySelector('#status span').textContent = 'Blocked';
+    }
+
 }
 
-btn.addEventListener('click',()=>{
-    getUser();
-})
+showStatus();
