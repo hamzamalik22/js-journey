588054bf22f48f64a958a6d77a3466f51433de94
hamzamalik22 <hamzamalikaj22@gmail.com>
2024-04-23 19:47:00 +0500

    Solving developer problems & concepts

---
+++ 6. Developer Problems/Script.js
@@ -0,0 +1,36 @@
+// 1. Debugging a type error
+
+// let obj = {};
+
+// obj.name; // will not show error
+
+// obj.name.age;  // will give a type error
+
+// obj?.name?.age; // A way to ignore type error
+
+// 2. Handling Asynchronous call
+
+let btn = document.querySelector('button');
+
+function getUser(){
+    fetch(`https://randomuser.me/api/`)
+    .then((raw) => raw.json())
+    .then((result) => {
+        const { name, email, gender, picture } = result.results[0];
+        document.querySelector(
+            "#parent"
+        ).innerHTML += `<div class="card w-60 p-4 rounded-xl bg-zinc-800">
+    <div class="w-20 h-20 bg-zinc-500 rounded-2xl mb-3 overflow-hidden">
+        <img src="${picture.large}" alt="">
+    </div>
+    <h3 class="font-semibold text-2xl">${name.first}</h3>
+    <h5 class="text-sm font-semibold opacity-60">${email}</h5>
+    <h3 class="text-sm opacity-40">${gender}</h3>
+    <h6 class="mt-5 text-xs font-semibold opacity-80">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime sunt dolor enim recusandae?</h6>
+</div>`;
+});
+}
+
+btn.addEventListener('click',()=>{
+    getUser();
+})
